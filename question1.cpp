0825950181bfc3953bedc7b3d2b132c5f9488ee7
xiaoyu-cmd <61233031+xiaoyu-cmd@users.noreply.github.com>
2020-02-19 19:34:38 +0800

    Create question1.cpp

---
+++ question1.cpp
@@ -0,0 +1,31 @@
+问题1
+bool wordBreak(string s, vector<string>& wordSet)
+{
+	if (s.empty())
+		return true;
+	if (wordSet.empty())
+		return false;
+
+	int validEnd = 0;
+	vector<bool> dp(s.size() + 1, false);
+	dp[0] = true;
+	for (int i = 0; i < s.size(); i++)
+	{
+		if (i == validEnd + 1) 
+			return false;
+		if (!dp[i]) 
+			continue;
+		for (auto& word : wordSet)
+		{
+			int newEnd = i + word.size();
+			if (newEnd > s.size()) 
+				continue;
+			if (memcmp(&s[i], &word[0], word.size()) == 0)
+			{
+				dp[newEnd] = true;
+				validEnd = max(validEnd, newEnd);
+			}
+		}
+	}
+	return dp.back();
+}
